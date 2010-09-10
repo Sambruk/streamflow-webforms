a9eb4c989ab14d3a0a742bdb3acc79a821a33410
Henrik Reinhold <henrik.reinhold@jayway.com>
2010-09-10 06:57:10 +0000

    Fixed problem that caused fields with empty date values not to show up in surface

--- web/src/main/webapp/js/app.js
+++ web/src/main/webapp/js/app.js
@@ -344,7 +344,7 @@ jQuery(document).ready(function()
         return value;
       } else {
         var d = value.match(/^(\d{4})-(\d{2})-(\d{2})[T ](\d{2}):(\d{2}):(\d{2}(?:\.\d+)?)(Z|(([+-])(\d{2}):(\d{2})))$/i);
-        if (!d) throw "ISODate, convert: Illegal format";
+        if (!d) return "Invalid date format";
         var dateValue = new Date(
           Date.UTC(d[1],d[2]-1,d[3],d[4],d[5],d[6]|0,(d[6]*1000-((d[6]|0)*1000))|0,d[7]) +
           (d[7].toUpperCase() ==="Z" ? 0 : (d[10]*3600 + d[11]*60) * (d[9]==="-" ? 1000 : -1000)));
