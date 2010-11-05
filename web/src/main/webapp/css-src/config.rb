2fc7ee70f978cdfcd6f504313636b11da0425746
Henrik Reinhold <henrik.reinhold@jayway.com>
2010-11-05 19:35:19 +0000

    Modifed the last page with a bigger caseid and and icon infront of the successmessage

--- web/src/main/webapp/css-src/config.rb
+++ web/src/main/webapp/css-src/config.rb
@@ -18,7 +18,7 @@
 # Require any additional compass plugins here.
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "../css"
+css_dir = "../../resources/files/css/"
 sass_dir = ""
 images_dir = "images"
 javascripts_dir = "js"
