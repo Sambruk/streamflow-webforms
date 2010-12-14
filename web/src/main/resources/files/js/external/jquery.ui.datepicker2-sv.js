32c2ac03490c6186dd03e9bf43ce98121a6c09a1
Mads Enevoldsen <mads.enevoldsen@jayway.com>
2010-12-14 08:41:27 +0000

    html encoded special characters

--- web/src/main/resources/files/js/external/jquery.ui.datepicker2-sv.js
+++ web/src/main/resources/files/js/external/jquery.ui.datepicker2-sv.js
@@ -1,18 +1,19 @@
+
 /* Swedish initialisation for the jQuery UI date picker plugin. */
 /* Written by Anders Ekdahl ( anders@nomadiz.se). */
 jQuery(function($){
     $.datepicker.regional['sv'] = {
-		closeText: 'Stäng',
-        prevText: '&laquo;Förra',
-		nextText: 'Nästa&raquo;',
+		closeText: 'St&auml;ng',
+        prevText: '&laquo;F&ouml;rra',
+		nextText: 'NÃ¤sta&raquo;',
 		currentText: 'Idag',
         monthNames: ['Januari','Februari','Mars','April','Maj','Juni',
         'Juli','Augusti','September','Oktober','November','December'],
         monthNamesShort: ['Jan','Feb','Mar','Apr','Maj','Jun',
         'Jul','Aug','Sep','Okt','Nov','Dec'],
-		dayNamesShort: ['Sön','Mån','Tis','Ons','Tor','Fre','Lör'],
-		dayNames: ['Söndag','Måndag','Tisdag','Onsdag','Torsdag','Fredag','Lördag'],
-		dayNamesMin: ['Sö','Må','Ti','On','To','Fr','Lö'],
+		dayNamesShort: ['S&ouml;n','M&aring;n','Tis','Ons','Tor','Fre','L&ouml;r'],
+		dayNames: ['S&ouml;ndag','M&aring;ndag','Tisdag','Onsdag','Torsdag','Fredag','L&ouml;rdag'],
+		dayNamesMin: ['S&ouml;','M&aring;','Ti','On','To','Fr','L&ouml;'],
 		weekHeader: 'Ve',
         dateFormat: 'yy-mm-dd',
 		firstDay: 1,
