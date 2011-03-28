8cc0a6862da83057be3ec1ea346f36d899942ce2
Rickard Öberg <rickard.oberg@jayway.com>
2011-03-28 15:13:46 +0800

    eID tester

--- web/src/main/java/se/streamsource/surface/web/SurfaceWebAssembler.java
+++ web/src/main/java/se/streamsource/surface/web/SurfaceWebAssembler.java
@@ -19,12 +19,7 @@ package se.streamsource.surface.web;
 
 import org.qi4j.api.common.Visibility;
 import org.qi4j.api.structure.Application;
-import org.qi4j.bootstrap.ApplicationAssembler;
-import org.qi4j.bootstrap.ApplicationAssembly;
-import org.qi4j.bootstrap.ApplicationAssemblyFactory;
-import org.qi4j.bootstrap.AssemblyException;
-import org.qi4j.bootstrap.LayerAssembly;
-import org.qi4j.bootstrap.ModuleAssembly;
+import org.qi4j.bootstrap.*;
 import org.qi4j.entitystore.memory.MemoryEntityStoreService;
 import org.qi4j.entitystore.prefs.PreferencesEntityStoreInfo;
 import org.qi4j.entitystore.prefs.PreferencesEntityStoreService;
@@ -39,35 +34,33 @@ import se.streamsource.surface.web.rest.SurfaceRestAssembler;
 
 import java.util.prefs.Preferences;
 
-import static org.qi4j.api.service.qualifier.ServiceTags.*;
-
 public class SurfaceWebAssembler
-   implements ApplicationAssembler
+        implements ApplicationAssembler
 {
-      private Object[] serviceObjects;
+   private Object[] serviceObjects;
 
-   public SurfaceWebAssembler( Object... serviceObjects )
+   public SurfaceWebAssembler(Object... serviceObjects)
    {
       this.serviceObjects = serviceObjects;
    }
 
-   public ApplicationAssembly assemble( ApplicationAssemblyFactory applicationFactory ) throws AssemblyException
+   public ApplicationAssembly assemble(ApplicationAssemblyFactory applicationFactory) throws AssemblyException
    {
       ApplicationAssembly assembly = applicationFactory.newApplicationAssembly();
-      assembly.setName( "Surface" );
-      assembly.setVersion( "0.3.20.962" );
-      LayerAssembly webLayer = assembly.layer( "Web" );
-      LayerAssembly appLayer = assembly.layer( "Application" );
-      LayerAssembly managementLayer = assembly.layer( "Management" );
-      LayerAssembly configLayer = assembly.layer("Configuration" );
+      assembly.setName("Surface");
+      assembly.setVersion("0.3.20.962");
+      LayerAssembly webLayer = assembly.layer("Web");
+      LayerAssembly appLayer = assembly.layer("Application");
+      LayerAssembly managementLayer = assembly.layer("Management");
+      LayerAssembly configLayer = assembly.layer("Configuration");
 
-      webLayer.uses( appLayer );
-      appLayer.uses( configLayer );
+      webLayer.uses(appLayer);
+      appLayer.uses(configLayer);
       managementLayer.uses(webLayer, appLayer, configLayer);
 
-      assembleWebLayer( webLayer );
+      assembleWebLayer(webLayer);
 
-      assembleAppLayer( appLayer);
+      assembleAppLayer(appLayer);
 
       assembleConfigLayer(configLayer);
 
@@ -75,79 +68,79 @@ public class SurfaceWebAssembler
 
       for (Object serviceObject : serviceObjects)
       {
-         assembly.setMetaInfo( serviceObject );
+         assembly.setMetaInfo(serviceObject);
       }
 
       return assembly;
    }
 
-   private void assembleManagementLayer( LayerAssembly managementLayer ) throws AssemblyException
+   private void assembleManagementLayer(LayerAssembly managementLayer) throws AssemblyException
    {
-      ModuleAssembly module = managementLayer.module( "JMX" );
+      ModuleAssembly module = managementLayer.module("JMX");
 
-      new JMXAssembler().assemble( module );
+      new JMXAssembler().assemble(module);
    }
 
-   private void assembleConfigLayer( LayerAssembly configLayer ) throws AssemblyException
+   private void assembleConfigLayer(LayerAssembly configLayer) throws AssemblyException
    {
-      ModuleAssembly module = configLayer.module( "Configurations" );
+      ModuleAssembly module = configLayer.module("Configurations");
 
-      module.entities( ClientConfiguration.class, ProxyConfiguration.class ).visibleIn( Visibility.application );
+      module.entities(ClientConfiguration.class, ProxyConfiguration.class).visibleIn(Visibility.application);
 
       // Configuration store
       Application.Mode mode = module.layer().application().mode();
-      if (mode.equals( Application.Mode.development ))
+      if (mode.equals(Application.Mode.development))
       {
          // In-memory store
-         module.services( MemoryEntityStoreService.class ).visibleIn( Visibility.layer );
-      } else if (mode.equals( Application.Mode.test ))
+         module.services(MemoryEntityStoreService.class).visibleIn(Visibility.layer);
+      } else if (mode.equals(Application.Mode.test))
       {
          // In-memory store
-         module.services( MemoryEntityStoreService.class ).visibleIn( Visibility.layer );
-      } else if (mode.equals( Application.Mode.production ))
+         module.services(MemoryEntityStoreService.class).visibleIn(Visibility.layer);
+      } else if (mode.equals(Application.Mode.production))
       {
          // Preferences storage
          ClassLoader cl = Thread.currentThread().getContextClassLoader();
-         Thread.currentThread().setContextClassLoader( null );
+         Thread.currentThread().setContextClassLoader(null);
          Preferences node;
          try
          {
-            node =  Preferences.userRoot().node( "streamsource/streamflow/surface" );
+            node = Preferences.userRoot().node("streamsource/streamflow/surface");
          } finally
          {
-            Thread.currentThread().setContextClassLoader( cl );
+            Thread.currentThread().setContextClassLoader(cl);
          }
 
-         module.services( PreferencesEntityStoreService.class ).setMetaInfo( new PreferencesEntityStoreInfo( node ) ).visibleIn( Visibility.layer );
+         module.services(PreferencesEntityStoreService.class).setMetaInfo(new PreferencesEntityStoreInfo(node)).visibleIn(Visibility.layer);
       }
    }
 
-   protected void assembleWebLayer( LayerAssembly webLayer ) throws AssemblyException
+   protected void assembleWebLayer(LayerAssembly webLayer) throws AssemblyException
    {
-      ModuleAssembly restModule = webLayer.module( "REST" );
-      new SurfaceRestAssembler().assemble( restModule );
-      new SurfaceResourceAssembler().assemble( restModule );
-      new ContextsAssembler().assemble( restModule );
+      ModuleAssembly restModule = webLayer.module("REST");
+      new SurfaceRestAssembler().assemble(restModule);
+      new SurfaceResourceAssembler().assemble(restModule);
+      new ContextsAssembler().assemble(restModule);
    }
 
-   private void assembleAppLayer( LayerAssembly appLayer ) throws AssemblyException
+   private void assembleAppLayer(LayerAssembly appLayer) throws AssemblyException
    {
-      ModuleAssembly proxyModule = appLayer.module( "Proxy" );
+      ModuleAssembly proxyModule = appLayer.module("Proxy");
 
-      proxyModule.services( ProxyService.class ).
-            visibleIn( Visibility.application ).
-            identifiedBy( "streamflowproxy" ).
-            setMetaInfo(tags("streamflow")).
-            instantiateOnStartup();
-      proxyModule.services( ProxyService.class ).
-            visibleIn( Visibility.application ).
-            identifiedBy( "eidproxy" ).
-            setMetaInfo( tags("eid" )).
-            instantiateOnStartup();
-      
-      proxyModule.services( ClientService.class ).
-            identifiedBy( "client" ).
-            instantiateOnStartup().
-            visibleIn( Visibility.application );
+      proxyModule.services(ProxyService.class).
+              visibleIn(Visibility.application).
+              identifiedBy("streamflowproxy").
+              taggedWith("streamflow").
+              instantiateOnStartup();
+      proxyModule.services(ProxyService.class).
+              visibleIn(Visibility.application).
+              identifiedBy("eidproxy").
+              taggedWith("eid").
+              instantiateOnStartup();
+
+      proxyModule.services(ClientService.class).
+              identifiedBy("client").
+              instantiateOnStartup().
+              visibleIn(Visibility.application);
    }
 }
