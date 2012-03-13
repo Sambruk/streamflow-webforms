99668e8f6838ed8fe922649ae61a469999587348
Arvid Huss <arvid.huss@jayway.com>
2012-03-13 15:23:30 +0100

    Generate license headers

--- web/src/main/resources/files/js/webforms-client.js
+++ web/src/main/resources/files/js/webforms-client.js
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 jQuery(document).ready(function()
 {
     function login( contextRoot, accesspoint ) {
